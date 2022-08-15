2ccf4825b89e0304ed36b20dbcbf36b2303f3005
Mr.Ben <42569624+1205BEN@users.noreply.github.com>
2022-08-15 22:14:05 +0700

    changes

--- admin/src/requestMethod.js
+++ admin/src/requestMethod.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
+// const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNGViNmY4Zjg0MTBhYTY1OTk0MTJhNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzI3NDMxNywiZXhwIjoxNjQzNTMzNTE3fQ.E9BdkE43b1wEYEvM3hCOZ5_eGJ7FhuH86RFqFP2sNoA'
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
